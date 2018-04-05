c361373ca71aa4bc79fd6b3b9e99ee6859149bb7
robotscissors <cjbazin@gmail.com>
2018-04-04 21:22:08 -0700

    Capture incoming text message and display
    Able to capture the body of the text message succesfully

--- app.rb
+++ app.rb
@@ -8,14 +8,18 @@ Envyable.load('./config/env.yml', 'development')
 require './app/models/model'
 require_relative './sms_machine.rb'
 
+include SmsMachine
+
 get '/' do
   'Sinatra has taken the stage.'
 end
 
 get '/send' do
-  @return_info = SmsMachine.send_sms(ENV['MY_CELL_PHONE_NUMBER'],ENV['TWILIO_PHONE_NUMBER'],"This is a test of a text!")
+  SmsFactory.send_sms(ENV['MY_CELL_PHONE_NUMBER'],ENV['TWILIO_PHONE_NUMBER'],"This is a test of a text!")
 end
 
 post '/sms' do
-  @send_info = SmsMachine.respond_sms("I am just testing the response")
+  number = params['From']
+  body = params['Body']
+  SmsFactory.respond_sms("What you sent me was: "+body)
 end
--- sms_machine.rb
+++ sms_machine.rb
@@ -1,19 +1,20 @@
 module SmsMachine
-
-  def self.send_sms(to_number, from_number, body_text)
-    @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"],ENV["TWILIO_AUTH_TOKEN"])
-    @client.messages.create(
-      to: to_number,
-      from: from_number,
-      body: body_text
-    )
-  end
-
-  def self.respond_sms(message_response)
-    twiml = Twilio::TwiML::Response.new do |r|
-      r.message message_response
+  class SmsFactory
+    def self.send_sms(to_number, from_number, body_text)
+      @client = Twilio::REST::Client.new(ENV["TWILIO_ACCOUNT_SID"],ENV["TWILIO_AUTH_TOKEN"])
+      @client.messages.create(
+        to: to_number,
+        from: from_number,
+        body: body_text
+      )
     end
-    #content_type 'text/xml'
-    twiml.text
+
+    def self.respond_sms(message_response)
+      twiml = Twilio::TwiML::Response.new do |r|
+        r.message message_response
+      end
+      #content_type 'text/xml'
+      twiml.text
+    end
   end
 end
