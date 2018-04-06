afc864dbee36acb6242a7607a82811984076b3e8
robotscissors <cjbazin@gmail.com>
2018-04-05 21:39:26 -0700

    Create a user model and insert new users
    Created a User model and check to see if this is a new
    or existing text. If it is new, then put it into the table of
    Users.

--- app.rb
+++ app.rb
@@ -5,13 +5,14 @@ require 'sinatra/activerecord'
 require './config/environments'
 require 'envyable'
 Envyable.load('./config/env.yml', 'development')
-require './app/models/model'
+require_relative './app/models/user.rb'
 require_relative './sms_machine.rb'
 
-include SmsMachine
+#include SmsMachine
 
 get '/' do
   'Sinatra has taken the stage.'
+
 end
 
 get '/send' do
@@ -19,7 +20,33 @@ get '/send' do
 end
 
 post '/sms' do
-  number = params['From']
-  body = params['Body']
-  SmsFactory.respond_sms("What you sent me was: "+body)
+
+  @number = params['From']
+  @body = params['Body']
+
+  #is a new user?
+  if User.exists?(:number => @number)
+    puts "this number exists!!"
+  else
+    @user = User.create(number: @number)
+    if @user.save
+      puts "save complete"
+    else
+      puts "error"
+    end
+  end
+
+  #stop automatically unsubscribes (how do we flag them in the database?)
+
+
+  #if not a new user then create a new user
+  # @user = User.create(number: @number)
+  #
+  # if @user.save
+  #     puts 'success'
+  # else
+  #     puts 'There was a problem'
+  # end
+
+  #SmsFactory.respond_sms("What you sent me was: "+body)
 end
