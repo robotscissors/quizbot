a8776c4ba5f80f578ad08ed0c055f0c6d0b8ce68
robotscissors <cjbazin@gmail.com>
2018-04-08 12:21:04 -0700

    Create a file to place constants
    In the Lib directory, I created a file to load that will have the
    phrases and constants for the quizbot.

---
+++ lib/constants.rb
@@ -0,0 +1,13 @@
+WELCOME = "Thank you for joining Keck Medicine of USC's quiz bot. "
+  + " All questions will result in True or False answers to which "
+  + "you can reply T or F. To get your score, reply to any message with "
+  + "keyword SCORE. To start another quick just type in that keyword. "
+  + "For a list of quizes, reply with LIST and a list of keywords and quizzes "
+  + "will be displayed. Good luck!"
+
+SCORE = "Your current score is: "
+
+ERROR_RESPONSE = "Hmmm. I don't recognize that response. Reply R to repeat the "
+  + "question, SCORE to get your score or LIST for listing of keywords and quizzes."
+
+    
